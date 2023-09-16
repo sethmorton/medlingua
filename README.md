339c3612af32d6f06b6ef731cc5c6539e98a93ad
Seth Morton <sethm@Seths-MacBook-Pro.local>
2023-09-16 15:01:53 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# MediLinguo - Real-Time Multilingual Medical Transcription
+
+MediLinguo is a revolutionary application that aims to provide real-time multilingual medical transcription services. The application uses advanced AI and NLP technologies to transcribe spoken medical consultations into written text, supporting multiple languages. Additionally, it employs generative AI to enhance transcription accuracy and efficiency.
+
+![MediLinguo Logo](path_to_your_logo.svg)
+
+## Table of Contents
+
+- [Features](#features)
+- [Getting Started](#getting-started)
+- [Tech Stack](#tech-stack)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Features
+
+- Real-time medical transcription from speech to text.
+- Multilingual support for transcription in various languages.
+- Enhanced transcription accuracy using generative AI.
+- Integration with InterSystems Supply Chain Orchestrator for secure data flow.
+
+## Getting Started
+
+1. Clone the repository: `git clone https://github.com/your-username/MediLinguo.git`
+2. Install the required dependencies by following the installation instructions in the project documentation.
+
+## Tech Stack
+
+- PostgreSQL with Lantern's vector database extension.
+- IBM Watson for natural language understanding and AI integration.
+- Generative AI for enhanced transcription.
+- InterSystems Supply Chain Orchestrator for secure data handling.
+
+## Usage
+
+1. Set up and configure PostgreSQL with Lantern's vector database extension by following the instructions in the [Lantern documentation](https://lantern.dev/docs).
+2. Integrate IBM Watson for natural language understanding and AI capabilities.
+3. Utilize generative AI to enhance the transcription process.
+4. Integrate with InterSystems Supply Chain Orchestrator for secure data flow within the healthcare system.
+
+## Contributing
+
+Contributions are welcome! Feel free to open issues or submit pull requests to contribute to this project. Make sure to follow the [Contributing Guidelines](CONTRIBUTING.md).
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+---
+
+For more information, visit our website [MediLinguo](https://medilinguo.example.com).
---
+++ src/routes/+page.svelte
@@ -0,0 +1,47 @@
+<div class="min-h-screen">
+    <nav class="absolute top-0 w-full bg-black bg-opacity-50 py-4">
+        <div class="container mx-auto flex justify-between items-center">
+          <!-- Logo -->
+          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" class="bi bi-file-earmark-text" viewBox="0 0 16 16">
+            <path d="M3 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2H3zm2 1h6v1H5V1zm0 2h6v1H5V3zm6 2H5v1h6V5zm0 2H5v1h6V7zm0 2H5v1h6V9zm0 2H5v1h6v-1zm-9-.786V13h8V11h1V2H4v6.214zM6 10h3V9H6v1z"/>
+          </svg>
+          
+          <a href="#" class="pl-2 text-white font-bold text-xl">MedLingua</a>
+          
+          <!-- Navbar Links -->
+          <div class="hidden md:flex flex-grow justify-end items-center space-x-6">
+            <a href="#" class="text-white">Home</a>
+            <a href="#" class="text-white">Features</a>
+            <a href="#" class="text-white">Services</a>
+            <a href="#" class="text-white">Contact</a>
+          </div>
+          
+          <!-- Responsive Navbar Toggle -->
+          <div class="flex md:hidden">
+            <button id="navbar-toggle" class="text-gray-800 focus:outline-none">
+              <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+        
+        <!-- Responsive Navbar Menu -->
+        <div id="navbar-menu" class="hidden md:hidden mt-2">
+          <a href="#" class="block py-2 px-4 text-indigo-600 font-bold">Home</a>
+          <a href="#" class="block py-2 px-4 text-indigo-600">Features</a>
+          <a href="#" class="block py-2 px-4 text-indigo-600">Services</a>
+          <a href="#" class="block py-2 px-4 text-indigo-600">Contact</a>
+        </div>
+      </nav>
+    <section class="flex flex-col justify-center items-center min-h-screen bg-gradient-to-r from-blue-500 to-indigo-600 text-white">
+        <div class="max-w-2xl text-center">
+          <h1 class="text-4xl md:text-6xl font-bold mb-4">Welcome to MedLingua</h1>
+          <p class="text-lg md:text-xl mb-8">Your dynamic solution for real-time multilingual medical transcription.</p>
+          <div class="flex justify-center">
+            <a href="#" class="mr-4 bg-white text-indigo-600 hover:bg-indigo-600 hover:text-white py-2 px-6 rounded-full transition duration-300 ease-in-out">Get Started</a>
+            <a href="#" class="bg-transparent border border-white text-white hover:bg-white hover:text-indigo-600 py-2 px-6 rounded-full transition duration-300 ease-in-out">Learn More</a>
+          </div>
+        </div>
+      </section>
+</div>
